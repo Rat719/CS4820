92ab79613a8a0d3a729f410a4bb45e270e38a3f5
chris <nipmucskater@gmail.com>
2026-01-25 20:35:58 -0500

    HW2: Almost done

---
+++ hw2.lisp
@@ -0,0 +1,304 @@
+(in-package "ACL2S")
+
+;; (modeling-validate-defs)
+;; (modeling-admit-defs)
+;; (modeling-admit-all)
+(modeling-start)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 1) SYNTAX: operators + SAEL expressions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Unary operators in SAEL: (- e) and (/ e)
+(defdata uoper (enum '(- /)))
+
+;; Binary operators in SAEL:  + , * , ^ ,
+(defdata boper (enum '(+ - * / ^)))
+
+(check= (boperp '*) t)
+(check= (boperp '^) t)
+(check= (boperp '!) nil)
+
+;; Mutually recursive def for SAEL expressions.
+;; - usaexpr is a unary: (uoper saexpr)
+;; - bsaexpr is a binary:(saexpr boper saexpr)
+(defdata
+  (saexpr (or rational
+              var
+              usaexpr
+              bsaexpr))
+  (usaexpr (list uoper saexpr))
+  (bsaexpr (list saexpr boper saexpr)))
+
+(check= (saexprp '((x + y) - (/ z))) t)
+(check= (saexprp '(x + y + z)) nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 2) ENVIRONMENTS: assignments + lookup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; assignment: vars to rationals
+(defdata assignment (alistof var rational))
+
+(check= (assignmentp '((x . 1) (y . 1/2))) t)
+(check= (assignmentp '((x 1) (y 1/2))) nil)
+
+;; lookup: return bound value if present, otherwise default to 1
+(definec lookup (v :var a :assignment) :rational
+  (if (endp a)
+      1
+      (let ((p (first a)))
+        (if (eq v (first p))
+            (rest p)
+            (lookup v (rest a))))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 3) ERRORS + RESULTS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defdata er 'error)
+(defconst *er* (nth-er-builtin 0))
+
+(defdata rat-err (or rational er))
+
+;; Convenience predicate: true iff value is not error
+(definec no-errorp (x :rat-err) :boolean
+  (not (equal x *er*)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 4) SEMANTICS: evaluator saeval
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Helper: combine two rat-err values with a rational->rational->rational op
+(definec binop-eval (op :boper x :rat-err y :rat-err) :rat-err
+  (if (or (equal x *er*) (equal y *er*))
+      *er*
+      (case op
+        (+ (+ x y))
+        (- (- x y))
+        (* (* x y))
+        (/ (if (equal y 0) *er* (/ x y)))
+        (^ (if (not (integerp y)) *er*
+               (if (and (equal x 0) (< y 0)) *er*
+                   (expt x y))))
+        (otherwise *er*))))
+;; The main evaluator
+(definec saeval (e :saexpr a :assignment) :rat-err
+  (match e
+         (:rational e)
+         (:var (lookup e a))
+         (:usaexpr
+          (('- x)
+           (let ((vx (saeval x a)))
+             (if (equal vx *er*) *er* (- vx))))
+          (('/ x)
+           (let ((vx (saeval x a)))
+             (if (equal vx *er*) *er*
+                 (if (equal vx 0) *er* (/ 1 vx))))))
+         (:bsaexpr
+          ((x '+ y) (binop-eval '+ (saeval x a) (saeval y a)))
+          ((x '- y) (binop-eval '- (saeval x a) (saeval y a)))
+          ((x '* y) (binop-eval '* (saeval x a) (saeval y a)))
+          ((x '/ y) (binop-eval '/ (saeval x a) (saeval y a)))
+          ((x '^ y) (binop-eval '^ (saeval x a) (saeval y a))))))
+
+(check= (saeval '((x + y) - (- z))
+                '((y . 3/2) (z . 1/2)))
+        3)
+
+(check= (saeval '(/ 0) nil) *er*)
+(check= (saeval '(1 / 0) nil) *er*)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5) PROPERTIES 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(property (a :assignment)
+  (== (saeval 'x a)
+      (saeval 'x a)))
+
+;; 1) -x = -(-(-x)), for var x
+(property (x :var a :assignment)
+          (== (saeval (list '- x) a)
+              (saeval (list '- (list '- (list '- x))) a)))
+
+;; 2) (x - y) = (x + (- y)), for vars x, y
+(property (x y :var a :assignment)
+          (== (saeval (list x '- y) a)
+              (saeval (list x '+ (list '- y)) a)))
+
+
+;; 3) (x * (y + z)) = ((x * y) + (x * z)) distributivity for saexpr x y z
+(property (x y z :saexpr a :assignment)
+          (== (saeval (list x '* (list y '+ z)) a)
+              (saeval (list (list x '* y) '+ (list x '* z)) a)))
+
+
+;; 4) (1 / (x / y)) = (y / x) for saexpr x y
+(property (x y :saexpr a :assignment)
+          (let ((valueX (saeval x a))
+                (valueY (saeval y a)))
+            (=> (and (no-errorp valueX)
+                     (no-errorp valueY)
+                     (not (equal 0 valueX))
+                     (not (equal 0 valueY)))
+                (== (saeval (list 1 '/ (list x '/ y)) a)
+                    (saeval (list y '/ x) a)))))
+
+;; 5) (0 ^ x) = 0 for saexpr x
+(property (x :saexpr a :assignment)
+          (let ((valueX (saeval x a)))
+
+            (=> (and (no-errorp valueX)
+                     (integerp valueX)
+                     (>= valueX 1))
+                (== (saeval (list 0 '^ x) a)
+                    0))))
+
+
+;; 6) (x ^ ((2 * y) / y)) = (x ^ 2) for saexpr x y
+(property (x y :saexpr a :assignment )
+          (let ( (valueX (saeval x a))
+                 (valueY (saeval y a)))
+            (=> (and (no-errorp valueX)
+                     (no-errorp valueY)
+                     (rationalp valueY)
+                     (not (zerop valueY)))
+                (== (saeval (list x '^
+                                  (list (list 2 '* y)
+                                        '/
+                                        y)) a)
+                    (saeval (list x '^ 2) a)))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6) AAEXPR / TRANSLATIONS / AAEVAL (placeholders)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; AAEL ::  rational | var | (- e) | (/ e) | (+ e e) | (- e e) | (* e e)
+;;          | (/ e e) | (expt e e)
+
+(defdata aaexpr
+    (or rational
+        var
+        (list '- aaexpr)
+        (list '/ aaexpr)
+        (list '+ aaexpr aaexpr)
+        (list '- aaexpr aaexpr)
+        (list '* aaexpr aaexpr)
+        (list '/ aaexpr aaexpr)
+        (list 'expt aaexpr aaexpr)))
+
+;; Converts a saexpr to an aaexpr
+(definec sael->aa (e :saexpr) :aaexpr
+  (match e
+         (:rational e)
+         (:var e)
+         (:usaexpr
+          (('- x) (list '- (sael->aa x)))
+          (('/ x) (list '/ (sael->aa x))))
+         (:bsaexpr
+          ((x '+ y) (list '+    (sael->aa x) (sael->aa y)))
+          ((x '- y) (list '-    (sael->aa x) (sael->aa y)))
+          ((x '* y) (list '*    (sael->aa x) (sael->aa y)))
+          ((x '/ y) (list '/    (sael->aa x) (sael->aa y)))
+          ((x '^ y) (list 'expt (sael->aa x) (sael->aa y))))))
+
+;; Converts an aaexpr to an saexpr
+;; kept getting unused variable conflicts here because of the match macro, so instead of turning it off I did
+;; a cond with an exhasutive search, using eq for object equality for the symbols
+(definec aa->sael (e :aaexpr) :saexpr
+  (cond
+    ((rationalp e) e)
+    ((varp e) e)
+
+    ;; unary
+    ((and (consp e) (equal (len e) 2) (eq (car e) '-))
+     (list '- (aa->sael (cadr e))))
+
+    ((and (consp e) (equal (len e) 2) (eq (car e) '/))
+     (list '/ (aa->sael (cadr e))))
+
+    ;; binary: (+ x y), (- x y), (* x y), (/ x y)
+    ((and (consp e) (equal (len e) 3) (eq (car e) '+))
+     (list (aa->sael (cadr e)) '+ (aa->sael (caddr e))))
+
+    ((and (consp e) (equal (len e) 3) (eq (car e) '-))
+     (list (aa->sael (cadr e)) '- (aa->sael (caddr e))))
+
+    ((and (consp e) (equal (len e) 3) (eq (car e) '*))
+     (list (aa->sael (cadr e)) '* (aa->sael (caddr e))))
+
+    ((and (consp e) (equal (len e) 3) (eq (car e) '/))
+     (list (aa->sael (cadr e)) '/ (aa->sael (caddr e))))
+
+    ;; exponent: (expt x y)
+    ((and (consp e) (equal (len e) 3) (eq (car e) 'expt))
+     (list (aa->sael (cadr e)) '^ (aa->sael (caddr e))))
+
+    (t
+     ;; should be unreachable if e :aaexpr
+     'x)))
+
+;; aa->sael is the inverse of sael->aa
+(property (x :saexpr)
+          (== (aa->sael (sael->aa x))
+              x ))
+
+;; sael->aa is the inverse of aa->sael
+(property (x  :aaexpr)
+          (== (sael->aa (aa->sael x))
+              x))
+
+;; evaluates an aaexpr to a rational
+(definec aaeval (e :aaexpr a :assignment) :rational
+  (match e
+         (:rational e)
+         (:var (lookup e a))
+
+         ;; unary minus
+         ((list '- x)
+          (- (aaeval x a)))
+
+         ;; unary reciprocal: division-by-0 => 0
+         ((list '/ x)
+          (let ((vx (aaeval x a)))
+            (if (equal vx 0) 0 (/ 1 vx))))
+
+         ;; binary ops
+         ((list '+ x y)
+          (+ (aaeval x a) (aaeval y a)))
+
+         ((list '- x y)
+          (- (aaeval x a) (aaeval y a)))
+
+         ((list '* x y)
+          (* (aaeval x a) (aaeval y a)))
+
+         ;; division-by-0 => 0
+         ((list '/ x y)
+          (let ((vy (aaeval y a)))
+            (if (equal vy 0) 0 (/ (aaeval x a) vy))))
+
+         ;; expt special cases
+         ((list 'expt x y)
+          (let ((vx (aaeval x a))
+                (vy (aaeval y a)))
+            (if (or (equal vy 0) (not (integerp vy)))
+                1
+                (if (equal vx 0)
+                    0
+                    (expt vx vy)))))))
+
+
+
+
+
+
+;; TODO: sael->aa, aa->sael
+;; TODO: inverse properties
+;; TODO: aaeval
+;; TODO: correspondence properties
